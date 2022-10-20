2a69234c3a38308490fc492671b97e7c879381a2
Grazziano Borges Fagundes <grazzianofagundes@gmail.com>
2022-10-20 11:40:06 -0300

    Criando o header principal

--- src/components/MainHeader/index.tsx
+++ src/components/MainHeader/index.tsx
@@ -1,11 +1,21 @@
-import React from 'react';
-
-import { Container } from './styles';
+import React, { useMemo } from 'react';
+import emojis from '../../utils/emojis';
+import { Container, Profile, UserName, Welcome } from './styles';
 
 const MainHeader: React.FC = () => {
+  const emoji = useMemo(() => {
+    const indice = Math.floor(Math.random() * emojis.length);
+    // console.log(indice);
+    return emojis[indice];
+  }, []);
+
   return (
     <Container>
-      <h1>MainHeader</h1>
+      <h1>Toggle</h1>
+      <Profile>
+        <Welcome>Olá, {emoji} </Welcome>
+        <UserName>Grazziano Fagundes</UserName>
+      </Profile>
     </Container>
   );
 };
