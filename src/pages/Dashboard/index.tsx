64365a8a7a6ba46f654959c7b5f0237773e16ce7
Grazziano Borges Fagundes <grazzianofagundes@gmail.com>
2022-10-21 13:10:58 -0300

    Criando select e deixando header content dinâmico

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import ContentHeader from '../../components/ContentHeader';
+import SelectInput from '../../components/SelectInput';
 import { Container } from './styles';
 
 const Dashboard: React.FC = () => {
+  const options = [
+    { value: 'Lara Croft', label: 'Lara Croft' },
+    { value: 'Jill Valentine', label: 'Jill Valentine' },
+    { value: 'Clare Redfield', label: 'Clare Redfield' },
+    { value: 'Leon S. Kennedy', label: 'Leon S. Kennedy' },
+  ];
+
   return (
     <Container>
-      <ContentHeader />
+      <ContentHeader title="Dashboard" lineColor="#f7931b">
+        <SelectInput options={options} />
+      </ContentHeader>
     </Container>
   );
 };
--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import ContentHeader from '../../components/ContentHeader';
+import SelectInput from '../../components/SelectInput';
+import { Container } from './styles';
 
 const List: React.FC = () => {
-  return <h1>List</h1>;
+  const options = [
+    { value: 'Lara Croft', label: 'Lara Croft' },
+    { value: 'Jill Valentine', label: 'Jill Valentine' },
+    { value: 'Clare Redfield', label: 'Clare Redfield' },
+    { value: 'Leon S. Kennedy', label: 'Leon S. Kennedy' },
+  ];
+
+  return (
+    <Container>
+      <ContentHeader title="Saídas" lineColor="#e44c4e">
+        <SelectInput options={options} />
+      </ContentHeader>
+    </Container>
+  );
 };
 
 export default List;
