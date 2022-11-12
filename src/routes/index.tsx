69591cef3211e3449a847f2213373796c08a655e
Grazziano Fagundes <grazzianofagundes@gmail.com>
2022-11-11 22:31:44 -0300

    Criando tela de login

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import AppRoutes from './app.routes';
+// import AppRoutes from './app.routes';
+import AuthRoutes from './auth.routes';
 import { BrowserRouter } from 'react-router-dom';
 
 const Routes: React.FC = () => {
   return (
     <BrowserRouter>
-      <AppRoutes />
+      {/* <AppRoutes /> */}
+      <AuthRoutes />
     </BrowserRouter>
   );
 };
